23e77995d0505fc712cde1141e2a9abe7f4faa52
Maeh <mariaeugeniamaciel@gmail.com>
2019-10-03 19:29:14 -0300

    esqueleto servidor node

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,30 +4,12 @@ const express = require('express')
 const app = express()
 const path = require('path');
 
-
-
-//gerenciamento de acesso cors (requisicao http bloqueada)
-//nao é bom usar muito
-app.use(function(req, res, next) {
-    res.header("Acess-Control-Allow-Origin", '*')
-    res.header("Acess-Control-Allow-Origin", 'Origin,X-Requested-With,Content-Type,Accept')
-    next()
-})
-
 app.use(express.static('static'));
 
-app.get('/data', (req, res) => {
-
-    res.json(filtro.resultado_pesquisa)
-
-
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+app.get('/lala', (req, res) => {
+    res.send('iae')
 })
 
 http.createServer(app).listen(porta, () => {
-
     console.log('Porta:' + porta)
 })
